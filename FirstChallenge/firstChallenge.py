26a54c2df2285f32f312c30068c6a5b9394e687c
Sam <60146956+srudqvist@users.noreply.github.com>
2021-12-07 11:59:27 -0600

    First Challenge completed

---
+++ FirstChallenge/firstChallenge.py
@@ -0,0 +1,28 @@
+### The first advent of code challenge
+
+
+# Get the input from the file
+def readData():
+    dataFile = open("./FirstChallenge/input.txt")
+    inputList = []
+    for line in dataFile:
+        thisLine = line.split(" ")
+        inputList.append(thisLine[0])
+    dataFile.close()
+    return inputList
+
+inputArray = readData()
+print(len(inputArray))
+
+
+count = 0
+isBiggerCount = 1
+for i in range(len(inputArray)):
+    count += 1
+    print(inputArray[i-1])
+    print(inputArray[i])
+    if inputArray[i] > inputArray[i - 1]:
+        isBiggerCount += 1
+
+print(isBiggerCount)
+print(count)
